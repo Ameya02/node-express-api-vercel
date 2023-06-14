6641bdc5677f02cccfa8a358aa93e6deb04124ca
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2023-06-14 09:14:41 +0530

    New Server

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+var cors = require("cors");
+const port = process.env.PORT || 4000;
+const connectDB = require("./db");
+connectDB();
+const serverRoutes = require("./api/serverRoutes");
+app.use([
+  cors(),
+  bodyparser.json(),
+  bodyparser.urlencoded({ extended: false }),
+]);
 
-const app = express()
-const PORT = 4000
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+app.use("/api/email", serverRoutes);
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
-
-// Export the Express API
-module.exports = app
+module.exports = app;
